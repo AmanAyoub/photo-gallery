8f7cac23c67e6779af637e7851330e35b63ca46e
Aman Ayoub <aman.ayoub16@gmail.com>
2026-03-12 22:22:08 +0430

    render comments and photo info

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,16 +1,43 @@
 import templates from "./templates.js";
 let API_URL = "http://localhost:3000";
+let photos;
+
+async function getPhotos() {
+  let response = await fetch(API_URL + "/photos");
+  let photosData = await response.json();
+
+  return photosData;
+}
+
+function setPhotos(photos) {
+  let photosDiv = document.getElementById("slides");
+  photosDiv.innerHTML = templates.photos(photos);
+}
+
+function renderPhotoInfo(photoId) {
+  let photo = photos.find(photo => photo.id === photoId);
+  let infoHeader = document.getElementById("information");
+  infoHeader.innerHTML = templates.photoInformation(photo);
+}
+
+async function fetchCommentsFor(photoId) {
+  let response = await fetch(API_URL + `/comments?photo_id=${photoId}`);
+  return await response.json();
+}
+
+async function renderCommentsFor(photoId) {
+  let comments = await fetchCommentsFor(photoId);
+  let list = document.querySelector("#comments ul");
+
+  list.innerHTML = templates.comments(comments);
+}
 
 document.addEventListener("DOMContentLoaded", async event => {
-  async function photosTemplate() {
-    let response = await fetch(API_URL + "/photos");
-    let photosData = await response.json();
+  photos = await getPhotos();
+  let activePhotoId = photos[0].id;
 
-    return templates.photos(photosData);
-  }
+  setPhotos(photos);
+  renderPhotoInfo(activePhotoId);
+  renderCommentsFor(activePhotoId);
 
-  let photos = await photosTemplate();
-  let photosDiv = document.getElementById("slides");
-  photosDiv.innerHTML = photos;
-  console.log(photosDiv);
 });
